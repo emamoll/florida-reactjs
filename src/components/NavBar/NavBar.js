ce5b126ad7b1234072e8352c104835cfd34233dd
emamoll <emanuelgmoll@gmail.com>
2022-01-19 22:27:30 -0300

    Agregue icono de carrito a la NavBar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,16 +1,19 @@
 import './NavBar.css';
 import Logo from '../Img/logo.png'
+import Carrito from '../Img/shopping-cart-solid.svg'
 
 const NavBar = () => {
     return (
         <header>
-            <a>
+            <a href="index.html">
                 <img src={Logo} alt="Logo Florida" className="logo"/>
             </a>
             <nav>
                 <li className="seccionesNavegador">Productos</li>|
                 <li className="seccionesNavegador">Favoritos</li>|
-                <li className="seccionesNavegador">Carrito</li>                
+                <li className="seccionesNavegador">
+                    <img src={Carrito} alt="Imagen Carrito" className="imagenCarrito"/>
+                </li>                
             </nav>
         </header>
     )
